5767a053e1d2bddce1330acfb4f997174b6f3c95
6704062611239 <s6704062611239@email.kmutnb.ac.th>
2025-12-23 15:05:06 +0700

    lab4no6.c display square of a character

--- displayacharacter.c
+++ displayacharacter.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main(){
-	int n1,n2,i;
+	int n1,n2,i,j;
 	scanf("%d %d",&n1,&n2);
 	for(i=0;i<n2;i++){
-		printf("%d",n1);
+		for(j=0;j<n2;j++){
+			printf("%d",n1);	
+		}
+		printf("\n");
 	}
 }
