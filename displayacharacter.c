e2d93ede679bb5a30b9d0a03533c50b780b19408
6704062611239 <s6704062611239@email.kmutnb.ac.th>
2025-12-23 15:07:38 +0700

    lab4no5.c display a character

--- displayacharacter.c
+++ displayacharacter.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 int main(){
-	int n1,n2,i,j;
+	int n1,n2,i;
 	scanf("%d %d",&n1,&n2);
 	for(i=0;i<n2;i++){
-		for(j=0;j<n2;j++){
-			printf("%d",n1);	
-		}
-		printf("\n");
+		printf("%d",n1);
 	}
 }
